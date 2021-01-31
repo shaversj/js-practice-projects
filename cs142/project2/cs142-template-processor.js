34c33528631f320762a17e607b227fca58c3986c
shaversj <shaversj@gmail.com>
2021-01-31 10:13:21 -0500

    cs142 project2 problem #3

--- cs142/project2/cs142-template-processor.js
+++ cs142/project2/cs142-template-processor.js
@@ -22,18 +22,18 @@ class Cs142TemplateProcessor {
         return tempTemplate;
     }
 }
-
-
-var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
-var dateTemplate = new Cs142TemplateProcessor(template);
-
-var dictionary = {month: 'July', day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary);
-
-console.log(str === 'My favorite month is July but not the day 1 or the year 2016');
-
-//Case: property doesn't exist in dictionary
-var dictionary2 = {day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary2);
-
-console.log(str === 'My favorite month is  but not the day 1 or the year 2016');
+//
+//
+// var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
+// var dateTemplate = new Cs142TemplateProcessor(template);
+//
+// var dictionary = {month: 'July', day: '1', year: '2016'};
+// var str = dateTemplate.fillIn(dictionary);
+//
+// console.log(str === 'My favorite month is July but not the day 1 or the year 2016');
+//
+// //Case: property doesn't exist in dictionary
+// var dictionary2 = {day: '1', year: '2016'};
+// var str = dateTemplate.fillIn(dictionary2);
+//
+// console.log(str === 'My favorite month is  but not the day 1 or the year 2016');
